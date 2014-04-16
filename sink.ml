8208a23384eafedef3f72b561bb6db97df5a43b1
Etienne Millon <me@emillon.org>
2014-04-16 22:37:55 +0200

    player indicators on sinks

--- sink.ml
+++ sink.ml
@@ -1,7 +1,14 @@
 open Tools
+open Types
 
-class sink parent ipos (player:Types.player) =
+class sink parent ipos player =
   let dom = div_class "sink" in
+  let extraclass = match player with
+  | P1 -> "sink-player1"
+  | P2 -> "sink-player2"
+  in
+  let player_indicator = div_class ~extraclass "sink-player" in
+  let _ = Dom.appendChild dom player_indicator in
   let (x, y) = ipos in
   let fpos = (float x, float y) in
   let _ = style_pos dom fpos in
