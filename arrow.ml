d97960d6838e6c625687234f0edb451995412de9
Etienne Millon <me@emillon.org>
2014-05-12 23:03:36 +0200

    simplify arrow

--- arrow.ml
+++ arrow.ml
@@ -11,19 +11,10 @@ class arrow parent (pos:int*int) dir player =
   let dom = div_class ~extraclass "arrow" in
   let _ = Dom.appendChild parent dom in
   object
-  val pos = pos
-  val dom = dom
-  val mutable dir = dir
-
   method pos = pos
-
-  method detach =
-    Dom.removeChild parent dom;
 
   method dir = dir
 
-  method turn =
-    dir <- dir_right dir;
-    let extraclass = dirclass "arrow" dir in
-    set_class dom ~extraclass "arrow"
+  method detach =
+    Dom.removeChild parent dom;
 end
